f6f2ff606ff987309afe7fe22f8c644cc27c780e
Lawitadelimon <lau72abrvjua@gmail.com>
2023-11-29 00:47:20 -0600

    05 Create the application’s HousingLocation component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <p>housing-location works!</p>
   `,
-  styles: [
-  ]
+  styleUrls: ['./housing-location.component.css'],
 })
-export class HousingLocationComponent {
+export class HousingLocationComponent {}
 
-}
